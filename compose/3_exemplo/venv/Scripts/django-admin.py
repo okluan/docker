f6eb7f1b83129947b6bbb85ec47548918fefa438
okluan <luanemerson8@gmail.com>
2024-07-07 12:02:18 -0300

    Alterações

---
+++ compose/3_exemplo/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Luan Lima\Desktop\github\3_docker\docker\compose\3_exemplo\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
